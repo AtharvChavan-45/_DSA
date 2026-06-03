73f099b6781c00adc0319723e624a956a10b93a0
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2026-06-03 02:41:08 +0000

    skip if value is neg and stop when value is zero

---
+++ 05_Array/stopZero_02.java
@@ -0,0 +1,26 @@
+// Create a program with a list of numbers where you want 
+// to skip negative values, but stop completely if you find a zero:
+import java.util.*;
+
+public class stopZero_02 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+
+        int[] arr = new int[num];
+        for(int i=0;i<num;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i : arr) {
+            if(i<0){
+                continue;
+            }
+            if(i==0){
+                break;
+            }
+           System.out.println(i);
+        }
+    }
+}
