50924adcb1e3ea41e44d8ca56573abffc1308216
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2026-06-03 02:29:11 +0000

    lowest value element in array

---
+++ 05_Array/LowestNum_01.java
@@ -0,0 +1,30 @@
+// Display lowest element in array
+
+import java.util.*;
+
+public class LowestNum_01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length of Array");
+        int num = sc.nextInt();
+
+        int[] arr = new int[num];
+
+        System.out.println("Enter Array Element :");
+        for(int i=0;i<num;i++){
+             arr[i] = sc.nextInt();
+        }
+
+        int lowestNum = arr[0];
+        for (int i : arr) {
+            if(lowestNum>i){
+                lowestNum = i;
+            }
+        }
+
+        System.out.println("Lowest number in arry: " + lowestNum);
+        
+    }
+}
