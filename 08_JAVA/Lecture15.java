1955ecc7afc45077e8a83b241baf22473701d348
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2026-08-06 11:11:03 +0000

    static and final keyword

--- 08_JAVA/Lecture15.java
+++ 08_JAVA/Lecture15.java
@@ -9,11 +9,19 @@ public class Lecture15 {
     public static void main(String[] args) {
         Student s1 = new Student("Aditya",18,101);
         Student s2 = new Student("Rohit",22,102);
+        Random r1 =  new Random();
+        System.out.print(r1.PI);
 
-        
-
+        final int x;
+        x=4;
+        System.out.print(x);
         System.out.println(s1.name + "," + s1.age + "," + s1.rollnumber + "," + Student.collage);
         System.out.println(s2.name + "," + s2.age + "," + s2.rollnumber + "," + s2.collage);
+
+        print();
+    } // all method in main class must be static to access it
+    static void print(){
+
     }
 }
 class Student {
@@ -34,3 +42,22 @@ class Student {
 
 }
 // Static can be variables, methods, inner class 
+
+//Final method can be variable, method, class and parameters
+
+class Random{
+    // final double PI = 3.14;
+    final double PI;
+    static final int x;
+
+    Random(){ // this also work
+        this.PI = 3.14;
+        // this.x = 4; this will not work
+    }
+    static {
+        x=4; // this will work
+    }
+    
+}
+
+// why main is static in java ? -> if main method is not static we require to make object of main class
